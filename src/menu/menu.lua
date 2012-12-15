8de09360b09ff61d69e6720b927de36a1c0ae2d0
Expugnosententia <djp1339@rit.edu>
2012-12-15 03:21:43 -0500

    Fonts fonts fonts

--- src/menu/menu.lua
+++ src/menu/menu.lua
@@ -2,9 +2,9 @@ menu = {}
 
 love.graphics.getFont()
 
-menu.font_title = love.graphics.newFont("menu/assets/Orbitron Black.ttf",64)
-menu.font_desc = love.graphics.newFont("menu/assets/League_Gothic-webfont.ttf",22)
-menu.font_menu = love.graphics.newFont("menu/assets/League_Gothic-webfont.ttf",26)
+menu.font_title = love.graphics.newFont("menu/assets/TriodPostnaja.ttf",64)
+menu.font_desc = love.graphics.newFont("menu/assets/Tower_Print.ttf",22)
+menu.font_menu = love.graphics.newFont("menu/assets/Tower_Print.ttf",26)
 menu.icon = love.graphics.newImage("menu/assets/icon.png")
 menu.bg = love.graphics.newImage("menu/assets/bg.png")
 menu.padding = 64
