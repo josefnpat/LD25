9ffb3136936e22ef41e757e5d0050954904c6ac0
Ingrid <hi@ingridcheung.com>
2012-12-15 20:28:48 -0500

    Closer.

--- src/entity/types/princess/princess.lua
+++ src/entity/types/princess/princess.lua
@@ -1,14 +1,9 @@
 local princess = {}
 
 function princess:draw()
-  local quad = love.graphics.newQuad(0, 0,
-             self.sprite.width,
-             self.sprite.height,
-             self.sprite.sheet:getWidth(),
-             self.sprite.sheet:getHeight())
-  love.graphics.drawq(self.sprite.sheet, quad,
+  love.graphics.drawq(self.sprite.sheet, self.quad,
              (self.camera.x * self.scale.x),
-             (self.camera.y * self.camera.y),
+             (self.camera.y * self.scale.y),
              0,
              self.scale.x,
              self.scale.y,
@@ -17,28 +12,41 @@ function princess:draw()
 end
 
 function princess:update(dt)
-  self.camera.x = (-camera.x + ((33 * 4) + self.x))
-  self.camera.y = (-camera.y + ((32 * 4) + self.y))
+  self.camera.x = (-camera.x + self.x)
+  self.camera.y = (-camera.y + self.y)
+  print ("!!!")
+  print (self.camera.x)
+  print (self.camera.y)
 end
 
 function princess.new()
   local prin = {}
   prin.type = "princess"
-  prin.x = 10
-  prin.y = 10
+--  prin.x = ((map.mapWidth / 2) + 0.5) * 16
+--  prin.y = ((map.mapHeight / 2) + 0.5) * 16
+  prin.x = player_obj.x
+  prin.y = player_obj.y
   prin.camera = {}
   -- Start next to player.
-  prin.camera.x = ((map.mapWidth / 2) + 0.5) * 16
-  prin.camera.y = ((map.mapHeight / 2) + 0.5) * 16
+  prin.camera.x = 0
+  prin.camera.y = 0
   prin.update = princess.update
   prin.draw = princess.draw
   prin.sprite = {}
   prin.sprite.sheet = love.graphics.newImage("entity/types/princess/princess.png")
+  prin.sprite.sheet:setFilter("nearest","nearest")
   prin.sprite.width = 16
   prin.sprite.height = 32
   prin.scale = {}
   prin.scale.x = 4
   prin.scale.y = 4
+
+  prin.quad = love.graphics.newQuad(0, 0,
+             prin.sprite.width,
+             prin.sprite.height,
+             prin.sprite.sheet:getWidth(),
+             prin.sprite.sheet:getHeight())
+
 
   return prin
 end
