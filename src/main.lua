7e8e9cc9e78159727e6aaa238a6d1056cd2cc80c
Expugnosententia <djp1339@rit.edu>
2012-12-15 19:36:53 -0500

    enemy table

--- src/main.lua
+++ src/main.lua
@@ -20,9 +20,13 @@ function love.load (arg)
   map.init()
   entity.load()
   player_obj = entity.new("player")
-  entity.new("enemy")
-  entity.new("enemy")
-  entity.new("enemy")
+ enemies = {}  
+  enemy1 = entity.new("enemy")
+  enemy2 = entity.new("enemy")
+  enemy3 = entity.new("enemy")
+   table.insert(enemies,enemy1)
+   table.insert(enemies,enemy2)
+   table.insert(enemies,enemy3)
   portal_enemy = entity.new("portal")
   portal_enemy.type = "portal_player"
   portal_enemy.x,portal_enemy.y = 10,10
