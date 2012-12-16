0273218779c06164183e27779ee3ff0dee8b3e76
Seppi <josefnpat@hotmail.com>
2012-12-16 03:04:47 -0500

    Remove comments

--- src/pathfinder/pathfinder.lua
+++ src/pathfinder/pathfinder.lua
@@ -6,30 +6,14 @@ pathfinder.stateObstacle = 0
 
 function pathfinder.find(map,start,stop)
 
-  -- If a start and end cell has been defined, use
-  -- the A* algorithm to find and display a path.
-
   if start then
     if stop then
-      --print("-- Calculating A* Path.")
       path = CalcPath(CalcMoves(map, start[1], start[2], stop[1], stop[2]))
       if path == nil then
-        --print("-- No path found.")
       else
-        --print("-- Path found:\n")
-        --for i,v in ipairs(path) do
-          --print(i,v)
-          --for j,w in pairs(v) do
-          --  print("",j,w)
-          --end
-        --end
         return path
       end
-    else
-      --print("-- ERROR: Path can't be found until end cell is selected.")
     end
-  else
-    --print("-- ERROR: Path can't be found until start cell is selected.")
   end
 end
 
