bc571a7db9528473f491df241611591fe60e95e5
VividReality <thevividreality@gmail.com>
2012-12-18 00:52:52 +0100

    dots on the map

--- src/mapLoad/minimap/minimap.lua
+++ src/mapLoad/minimap/minimap.lua
@@ -8,7 +8,6 @@ minimap.madeCanvas = false
 minimap.backdrop = love.graphics.newImage("mapLoad/minimap/img/minimap.png")
 minimap.backdrop:setFilter("nearest","nearest")
 
-
 function minimap.draw()
 if minimap.madeCanvas == false then
 love.graphics.setCanvas(minimap.miniCanvas)
@@ -30,6 +29,23 @@ love.graphics.draw(minimap.backdrop,10,10,0,4,4)
 love.graphics.setColor(255,255,255,150)
 love.graphics.draw(minimap.miniCanvas,25,25,0,2.5,2.5)
 love.graphics.setColor(255,255,255)
+  for i,v in ipairs(entity.data) do
+      local x,y = entity.RawToMap(v)
+		if v.type == "portal" then
+		love.graphics.setColor(255,255,0)
+        elseif v.type == "slowtrap" or v.type == "spiketrap" then
+        love.graphics.setColor(0,255,255)
+        elseif v.type == "player" then
+        love.graphics.setColor(0,0,255)
+        elseif v.type == "enemy" then
+        love.graphics.setColor(255,0,0)
+        else
+        love.graphics.setColor(255,255,255)
+		end
+		
+		love.graphics.rectangle("fill",25 + x * 2.5,25 + y * 2.5,2,2)
+		love.graphics.setColor(255,255,255)
+  end
 end
 
 return minimap
