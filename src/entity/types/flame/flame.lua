e7b19ba26e8cb1f34e53d3eeca55e9fee28123f0
VividReality <thevividreality@gmail.com>
2012-12-17 20:03:28 +0100

    needed files for flames

---
+++ src/entity/types/flame/flame.lua
@@ -0,0 +1,51 @@
+local flame = {}
+flame.speed = 250
+
+
+function flame:update(dt)
+previousX = self.x
+previousY = self.y
+self.dt = self.dt + dt
+if self.dir == "down" then
+self.y = self.y + flame.speed*dt
+self.angle = math.rad(90)
+end
+if self.dir == "right" then
+self.x = self.x + flame.speed*dt
+self.angle = math.rad(0)
+end
+if self.dir == "up" then
+self.y = self.y - flame.speed*dt
+self.angle = math.rad(270)
+end
+if self.dir == "left" then
+self.x = self.x - flame.speed*dt
+self.angle = math.rad(180)
+end
+
+if entity.collision(self) then
+    self.x = previousX
+    self.y = previousY
+  end
+end
+
+function flame:draw()
+local x,y = entity.getScreenLocation(self)
+love.graphics.drawq(map.graphics.sheet,map.quads[57],x,y,self.angle,4,4)
+end
+
+function flame.new()
+  local e = {}
+  e.type = "flame"
+  e.img = map.graphics.sheet
+  e.angle = 0
+  e.quad = 57
+  e.dt = 0
+  e.x,e.y = 0,0
+  e.draw = flame.draw
+  e.update = flame.update
+  e.dir = "down"
+  return e
+end
+
+return flame
